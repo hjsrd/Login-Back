fe764bd2869faf2045efbb59e2cee8783bc22360
hjsrd <hjmon10@gmail.com>
2026-08-03 21:49:34 +0330

    echo "# Samuraei-login-page" >> README.md git init git add README.md git commit -m "first commit" git branch -M main git remote add origin https://github.com/hjsrd/Samuraei-login-page.git git push -u origin main

---
+++ script.js
@@ -0,0 +1,8 @@
+const btn = document.getElementById("toggle");
+const pass = document.getElementById("password");
+
+btn.addEventListener("click", () => {
+  const show = pass.type === "password";
+  pass.type = show ? "text" : "password";
+  btn.textContent = show ? "hide" : "show";
+});
